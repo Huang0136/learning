0b8db424bcce806ad63df22ec4afa55a7168c30d
Huang0136 <2010hgh@gmail.com>
2016-04-18 19:24:58 +0800

    添加Spring MVC支持。

--- src/main/java/com/huang/controller/IndexController.java
+++ src/main/java/com/huang/controller/IndexController.java
@@ -1,5 +1,29 @@
 package com.huang.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Index控制器
+ * @author huanggh
+ * @since 2016-04-18 19:01
+ */
+@Controller
 public class IndexController {
 	
+	private static final Logger logger = Logger.getLogger(IndexController.class);
+	
+	/**
+	 * 主页
+	 * @return
+	 */
+	@RequestMapping(value={"/","","/index"}, method = RequestMethod.GET)
+	public String index() {
+		
+		logger.info("indexController...");
+		
+		return "index";
+	}
 }

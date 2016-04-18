a5c38ffcd70130a3fb59c85bd4bc5418f0b47617
Huang0136 <2010hgh@gmail.com>
2016-04-18 16:47:41 +0800

    user controller service

---
+++ src/main/java/com/huang/bean/User.java
@@ -0,0 +1,10 @@
+package com.huang.bean;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -8524804429422960523L;
+	
+	
+}
---
+++ src/main/java/com/huang/service/impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.huang.service.impl;
+
+import com.huang.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+}
